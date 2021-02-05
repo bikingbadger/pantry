6f8feaee4a87d2bc25abfd0ea0fa16144cafe3c1
Hilton Meyer <development@hiltonmeyer.com>
2021-02-05 18:13:54 +0200

    feat(ingr): Extract component

---
+++ src/components/ingredient/AddIngredient.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="ingredient">
+    <div class="add-ingredient-frm">
+      <div>
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" />
+      </div>
+      <div>
+        <label for="category">Category:</label>
+        <select id="category" name="category">
+          <option value="legumes">Legumes</option>
+          <option value="vegetables">Vegetables</option>
+          <option value="fruit">Fruit</option>
+          <option value="dairy">Dairy</option>
+        </select>
+      </div>
+      <div>
+        <label for="uom">Unit of Measure:</label>
+        <input type="text" name="uom" id="uom" />
+      </div>
+      <div><button>Add</button></div>
+    </div>
+    <ul>
+      <li>Milk</li>
+      <li>Whole wheat flour</li>
+      <li>Olive Oil</li>
+    </ul>
+  </div>
+</template>
