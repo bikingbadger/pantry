9af853f0fbbd6b79a789856d1c09e6b406c78526
Hilton Meyer <development@hiltonmeyer.com>
2021-02-05 18:20:40 +0200

    feat(setup): Add firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import firebase from 'firebase/app';
 
-createApp(App).use(router).mount('#app')
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyDbYPvxMaECPwjgR06njRfTLFa_skZ9-Qo',
+  authDomain: 'pantry-fe77c.firebaseapp.com',
+  databaseURL: 'https://pantry-fe77c-default-rtdb.firebaseio.com',
+  projectId: 'pantry-fe77c',
+  storageBucket: 'pantry-fe77c.appspot.com',
+  messagingSenderId: '235929136377',
+  appId: '1:235929136377:web:23a498fc887466ce76c628',
+  measurementId: 'G-S4ER2JYTKZ',
+};
+firebase.initializeApp(firebaseConfig);
+
+createApp(App)
+  .use(router)
+  .mount('#app');
