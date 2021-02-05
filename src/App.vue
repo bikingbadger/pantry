685a76f97fb168153e57e7da26e72f8fcdfff449
Hilton Meyer <development@hiltonmeyer.com>
2021-02-05 12:40:00 +0200

    feat(ingr): Add ingredient form

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
+  <img
+    alt="App logo"
+    src="https://res.cloudinary.com/hiltonmeyer-com/image/upload/v1611488325/pantry/cooking-robot_xdaw7k.png"
+  />
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/ingredients">Ingredients</router-link>
   </div>
   <router-view />
 </template>
@@ -17,12 +21,17 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: var(--clr-neutral-500);
+}
+
+img {
+  display: block;
+  margin: 0 auto;
 }
 
 #nav {
   padding: 30px;
+  text-align: center;
 }
 
 #nav a {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="home">
-    <img
-      alt="App logo"
-      src="https://res.cloudinary.com/hiltonmeyer-com/image/upload/v1611488325/pantry/cooking-robot_xdaw7k.png"
-    />
+    
     <h1>Pantry</h1>
     <h2>Kitchen Manager</h2>
   </div>
