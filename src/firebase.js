16377f1afd4a7e8cb3d2138fdbceb96afc82263c
Hilton Meyer <development@hiltonmeyer.com>
2021-03-03 09:48:51 +0200

    feat(auth): Add register option

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,18 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyDbYPvxMaECPwjgR06njRfTLFa_skZ9-Qo",
-    authDomain: "pantry-fe77c.firebaseapp.com",
-    databaseURL: "https://pantry-fe77c-default-rtdb.firebaseio.com",
-    projectId: "pantry-fe77c",
-    storageBucket: "pantry-fe77c.appspot.com",
-    messagingSenderId: "235929136377",
-    appId: "1:235929136377:web:f4687227f50dc7bd76c628",
-    measurementId: "G-7J6VBCW3SE"
-  };
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
-  export default firebaseConfig;
+const firebaseConfig = {
+  apiKey: 'AIzaSyDbYPvxMaECPwjgR06njRfTLFa_skZ9-Qo',
+  authDomain: 'pantry-fe77c.firebaseapp.com',
+  databaseURL: 'https://pantry-fe77c-default-rtdb.firebaseio.com',
+  projectId: 'pantry-fe77c',
+  storageBucket: 'pantry-fe77c.appspot.com',
+  messagingSenderId: '235929136377',
+  appId: '1:235929136377:web:f4687227f50dc7bd76c628',
+  measurementId: 'G-7J6VBCW3SE',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
